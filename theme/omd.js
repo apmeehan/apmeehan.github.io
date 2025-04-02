b88297f3cb04c1db00a16a019adf0bcc7b8ab596
Pete Meehan <apetermeehan@gmail.com>
2025-04-03 00:48:21 +0100

    remove lighthouse theme

--- theme/omd.js
+++ theme/omd.js
@@ -15,34 +15,35 @@ newLinksContainer.id = "links-grid-container";
 newLinksContainer.appendChild(newLinksInnerContainer);
 
 const linkPositions = [
-  { col: 2, row: 1 },
-  { col: 6, row: 3 },
-  { col: 3, row: 7 },
+  [3, 2],
+  [7, 4],
+  [4, 8],
 ];
 
-for (let row = 0; row < GRID_ROW_COUNT; row += 1) {
+for (let row = 1; row <= GRID_ROW_COUNT; row += 1) {
   const divRow = document.createElement("div");
   divRow.className = "grid-row";
 
   newLinksInnerContainer.appendChild(divRow);
 
-  for (let col = 0; col < GRID_COLUMN_COUNT; col += 1) {
+  for (let col = 1; col <= GRID_COLUMN_COUNT; col += 1) {
     const divCell = document.createElement("div");
     divCell.className = "grid-cell";
     divRow.appendChild(divCell);
 
-    const linkIndex = linkPositions.findIndex((p) => p.row === row && p.col === col);
-
-    if (linkIndex > -1) {
-      const link = linksList.children[linkIndex].querySelector("a");
-      link.textContent = '';
-
-      divCell.appendChild(link);
+    const linkIndex = linkPositions.findIndex(([i, j]) => j === row && i === col);
+    
+    if (linkIndex !== -1 && linkIndex < linksList.children.length) {
+      const linkElement = linksList.children[linkIndex];
+      if (linkElement) {
+        const link = linkElement.querySelector("a");
+        if (link) {
+          link.textContent = '';
+          divCell.appendChild(link);
+        }
+      }
     }
   }
 }
 
 linksList.replaceWith(newLinksContainer);
-
-// Add link to design source
-document.querySelector("footer").appendChild(Object.assign(document.createElement("a"), { textContent: "Design source", href: "https://www.udiscovermusic.com/stories/omd-orchestral-manoeuvres-in-the-dark-debut-album" }))
